3c14b1222f573d51c88619a3298769ff97ac8421
8an3 <skylerzanth@gmail.com>
2024-03-07 22:13:24 -0500

    kjhb

--- app/routes/_auth.logout.tsx
+++ app/routes/_auth.logout.tsx
@@ -2,28 +2,44 @@ import { requireUserSession } from "~/helpers";
 //import { authenticator } from "~/services";
 import { prisma } from "~/libs";
 import { type LoaderFunctionArgs, type ActionArgs, json, redirect } from '@remix-run/node'
-import { getSession as sessionGet, getUserByEmail } from '~/utils/user/get'
+import { getSession, destroySession } from '../sessions/auth-session.server';
+import { model } from '../models'
 
 
 
 
 export async function action({ request }: ActionArgs) {
-  const userSession = await sessionGet(request.headers.get("Cookie"));
+  const session = await getSession(request.headers.get("Cookie"));
 
-  if (!userSession) { return json({ status: 302, redirect: '/login' }); };
+  if (!session) { return json({ status: 302, redirect: '/login' }); };
 
-  if (!userSession) { return redirect('/login') }
-  const email = userSession.get("email")
-  const user = await getUserByEmail(email)
-  if (!user) { return json({ status: 302, redirect: '/login' }) };
-
+  if (!session) { return redirect('/login') }
+  const email = session.get("email")
+  const user = await model.user.query.getForSession({ email: email });
+  if (!user) {
+    return json({ status: 302, redirect: '/login' })
+  }
+  else {
+    return redirect('/emails', {
+      headers: {
+        "Set-Cookie": await destroySession(session),
+      },
+    });
+  }
 }
-export async function loader({ request, params }: LoaderFunctionArgs) {
-  const userSession = await authenticator.isAuthenticated(request, { failureRedirect: "/login", }); const user = await model.user.query.getForSession({ id: userSession.id });
 
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const email = session.get("email")
+
+  const user = await model.user.query.getForSession({ email: email });
   if (!user) {
     return redirect('/login')
   } else {
-    return redirect('/emails')
+    return redirect('/emails', {
+      headers: {
+        "Set-Cookie": await destroySession(session),
+      },
+    });
   }
 }
