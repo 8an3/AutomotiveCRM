b9077f56600040df9624db27473c48ce1eca2d84
8an3 <skylerzanth@gmail.com>
2024-06-09 21:54:55 -0400

    aasdf

--- app/routes/api/cron.js
+++ app/routes/api/cron.js
@@ -3,8 +3,14 @@ export const config = {
 };
 
 export async function loader({ req }) {
+  const why = 'why me'
+  const test = await prisma.automations.create({
+    data: {
+      userEmail: 'skylerzanth@gmail.com',
+      pickUp24before: 'yes',
+      appt24before: 'yes',
+    }
+  })
 
-
-
-  return new Response('Hello Cron!');
+  return new Response('Hello Cron, fuk you !') && test;
 }
